80301dff9df5bd28278315b91822e9cb2eaebf0c
Ailton Filho <ailtonxdz@gmail.com>
2023-05-17 18:46:54 -0300

    Adicionado add book page

--- src/assets/styles/AddBookStyle.js
+++ src/assets/styles/AddBookStyle.js
@@ -9,3 +9,35 @@ export const Card = styled.div`
 export const Title = styled.h2`
     margin-top: 4rem;
 `;
+
+export const SbmtBtn = styled.input`
+    background-color: #6CD05C ;
+    border-radius: 500px ;
+    border: none;
+    :focus{
+        background-color: #5db450;
+    }
+`;
+
+export const TextMuted = styled.span`
+    color: #B1B1B1;
+`;
+
+export const FileInput = styled.label`
+    border: 1px solid #727CF5;
+    padding: 1.625rem 2.5rem;
+    color: #727CF5;
+    cursor: pointer;
+    :hover{
+        background-color: #727CF5;
+        color: white;
+    }
+`;
+
+export const FileIcon = styled.i`
+    font-size: 4rem;
+`;
+
+export const TinyText = styled.span`
+    font-size: .75rem;
+`;
--- src/pages/AddBook.js
+++ src/pages/AddBook.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header';
-import {Card, Title} from '../assets/styles/AddBookStyle';
+import Footer from '../components/Footer';
+import {Card, FileIcon, FileInput, SbmtBtn, TextMuted, TinyText, Title} from '../assets/styles/AddBookStyle';
 
 export function AddBook(props){
     return (
@@ -9,23 +10,50 @@ export function AddBook(props){
                 <Title className='title fw-bold'>Anunciar livro</Title>
                 <Card className='card border-0 shadow-lg'>
                     <form>
-                        <div class="mb-3">
-                            <label for="book-title" class="form-label">Título do livro</label>
-                            <input type="text" class="form-control" name="book-title"/>
+                        <div className="mb-4">
+                            <label for="book-title" className="form-label">Título do livro</label>
+                            <input type="text" className="form-control" name="book-title"/>
                         </div>
-                        <div class="mb-3">
-                            <label for="category" class="form-label">Categoria</label>
-                            <select class="form-select" name="category" aria-label="Selecione a categoria">
+                        <div className="mb-4">
+                            <label for="category" className="form-label">Categoria</label>
+                            <select className="form-select" name="category" aria-label="Selecione a categoria">
                                 <option selected disabled>Selecione a categoria</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
                                 <option value="3">Three</option>
                             </select>
                         </div>
-                        <a className='sbmt-btn'>teste</a>
+                        <div className="mb-4">
+                            <label for="book-author" className="form-label">Título do livro</label>
+                            <input type="text" className="form-control" name="book-author"/>
+                        </div>
+                        <div className="mb-4">
+                            <label for="book-price" className="form-label">Título do livro</label>
+                            <input type="text" className="form-control" name="book-price"/>
+                        </div>
+
+                        <div className="mb-4">
+                            <label for="desc" className="form-label">Descrição</label>
+                            <textarea className="form-control" id="desc" rows="5" placeholder='Ex: Bom estado, impresso em 2001...'></textarea>
+                        </div>
+
+                        <div className="mb-4">
+                            <p>Fotos <TextMuted>(até 4 fotos)</TextMuted></p>
+
+                            <FileInput for="formFile" className="form-label rounded text-center">
+                                <FileIcon className='ri-image-add-line'></FileIcon>
+                                <p className='fw-bold m-0'>Adicionar fotos</p>
+                                <TinyText>Apenas em JPG e PNG</TinyText>
+                            </FileInput>
+
+                            <input className="d-none form-control" type="file" id="formFile" />
+                        </div>
+
+                        <SbmtBtn className="text-white px-5 py-2" type="submit" value="Enviar"/>
                     </form>
                 </Card>
             </div>
+            <Footer />
         </div>
     );
 }
