d50cb5ca610991ad549069617d08adec16673262
PlanoCartesiano <scalesteam354@gmail.com>
2023-06-07 00:20:34 -0300

    construção da pagina e do style book

---
+++ src/assets/styles/BookStyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const BlockTest = styled.div`
+    width: 898px;
+    height: 606px;
+    background-color: gray;
+    border-radius: 10px;
+`;
+
+export const BuyBtn = styled.input`
+    background-color: #6CD05C;
+    width: 150px;
+    height: 40px;
+    border-radius: 500px;
+
+    :focus{
+        background-color: #5db450;
+    }
+`;
